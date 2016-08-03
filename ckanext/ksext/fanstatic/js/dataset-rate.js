0cac69109ea449ce6eedd94ccbcd7fcaaccfda66
WilJoey Lin <wiljoey@gmail.com>
2016-08-03 10:41:32 +0800

    add jRate js

---
+++ ckanext/ksext/fanstatic/js/dataset-rate.js
@@ -0,0 +1,18 @@
+﻿$(function() {
+    var jr = $("#jRate").jRate({
+        startColor: 'cyan',
+        endColor: 'blue',
+        width: 60,
+        height: 60,
+        rating: 5,
+        count: 5,
+        precision: 1,
+        readOnly: false,
+        onSet: function(rating) {
+            console.log('onSet');
+        }
+    });
+    console.log(jr.getRating());
+    jr.setRating(1);
+    jr.setReadOnly(true);
+});
